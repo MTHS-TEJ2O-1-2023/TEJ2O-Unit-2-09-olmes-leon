36f3ccfc21dcce3207b625f7811c23664d18a45d
olmes-leon <144264100+olmes-leon@users.noreply.github.com>
2023-11-16 12:41:19 -0500

    code

--- main.ts
+++ main.ts
@@ -25,36 +25,36 @@ input.onButtonPressed(Button.B, function () {
 })
 
 input.onGesture(Gesture.Shake, function () {
-    randomNumber = randint(0, 2)
-    basic.clearScreen()
-    // if randomNumber was 0
-    if (randomNumber == 0) {
-        basic.showIcon(IconNames.Scissors)
-    }
+  randomNumber = randint(0, 2)
+  basic.clearScreen()
+// if randomNumber was 0
+  if (randomNumber == 0) {
+    basic.showIcon(IconNames.Scissors)
+  }
 
-    // if randomNumber was 1
+// if randomNumber was 1
 
-    if (randomNumber == 1) {
-        basic.showLeds(`
-    . . . . .
-    . # # # .
-    . # # # .
-    . # # # .
-    . . . . .
-    `)
-    }
+  if (randomNumber == 1) {
+    basic.showLeds(`
+  . . . . .
+  . # # # .
+  . # # # .
+  . # # # .
+  . . . . .
+  `)
+  }
 
-    // if randomNumber was 2
-    if (randomNumber == 2) {
-        basic.showLeds(`
-    # # # # #
-    # . . . #
-    # . . . #
-    # . . . #
-    # # # # #
-    `)
-    }
-    // pause and show you are ready again
-    basic.pause(5000)
-    basic.showIcon(IconNames.Happy)
+// if randomNumber was 2
+  if (randomNumber == 2) {
+    basic.showLeds(`
+  # # # # #
+  # . . . #
+  # . . . #
+  # . . . #
+  # # # # #
+  `)
+  }
+// pause and show you are ready again
+  basic.pause(5000)
+  basic.showIcon(IconNames.Happy)
 })
